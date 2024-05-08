ad7c64c76120e92e911b7ae1c4ed1b2b4d6f8c58
yeahbudd01 <106033471+yeahbudd01@users.noreply.github.com>
2024-05-08 02:42:41 -0700

    Updated prime time code with some advised code.
    Also commented on how prime time code works. still working on the 'getting loopy' code.

--- lab308-3.mjs
+++ lab308-3.mjs
@@ -13,18 +13,24 @@ for(let i = 1; i <= 100; i++){
 
 // Part 2 'Prime Time'
 
-let n = 17; //set arbitary number of n
-let i = n + 1
+let n = 20; //set arbitary number of n
+let num = n //set number counter to reference one plus n
 let prime = false; //set up boolean value to check if number is prime
 
-while(prime = false){ //set up while loop for i being a counter that will count to the next number
-    count = 2;
-    i++;
-    if(i % count == 0){
-        prime = false;
-    } else {prime = true;}
+
+while(prime != true){ //set up while loop for when prime is not true.
+    num++;//start num count to check a few numbers after n.
+    prime = true; //debugger found this to be problematic. I'm not sure why, but I initially put this at the end of the loop. 
+    for(let i = 2; i <= 10; i++) //initially I wanted to do an if statement first, and nest a for loop within it, but it got a bit complicated. I was advised to put the for loop above the if statement.
+    {//for loop that would create the variable i, so that i can be checked with num to find a remainder. i will continue to increase by one until it reaches ten or if...
+        if(num % i === 0){ //if num % i is exactly 0, then i will stop counting
+            prime = false; // then the prime bool will switch back to false
+            break; //break out of the nested for loop, since the for loop has done it's job to find a number (i) that is divisible by the var num other than 1. 
+    }
+    //restart the loop, because prime is still not true. the for loop will conclude after i has reached 10. afterwards, if no number is divisible, prime will equal true, and the while loop will end.
+    }
 }
-console.log(i);
+console.log(`The next prime number after ${n} is ${num}`);
 
 // Part 3 'Feeling Loopy' 
 //create a grid of four cells, that contain strings. use an empty string variable c, to go over the csv string and then log everything in the comma into a cell.
@@ -36,14 +42,20 @@ let cell2='';
 let cell3 ='';
 let cell4 ='';
 let c =''; //empty string reader
-const csv = `ID,Name,Occupation,Age`;
+const csv = "ID,Name,Occupation,Age\n42,";
 
-//try to do a for loop within a for loop. 
-for (c of csv) { //for loop with string c of csv reading through the string.
+//try to do a for loop within a for loop. because I want to stop after the comma, but continue with the string until all cells are filled. 
+// the main for loop should look something like this: for (let i = 0; i < csv.length; i++)
+
+
+
+for (c of csv) { //for loop with string c of csv reading through the string csv.
     if (c !== `,`) { //have the string c go through the csv string until it reaches a comma.
         cell1 += c; //Concatenate and asign cell1 with string c. 
         c = ``; //reset string c to zero.
-    }
-}
+    } else{break;}
 
-console.log(cell1)
+}
+   
+
+console.log(cell1, cell2);
