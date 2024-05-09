238e8f9e64a263c50a776160d7ba49156f9833ac
yeahbudd01 <106033471+yeahbudd01@users.noreply.github.com>
2024-05-08 20:59:04 -0700

    updated the feeling loopy loop with an if statement and nested if statement.
    I was overthinking the logic here. I thought I needed a nested loop for the csv portion. But was given more perspective on the logic and what to look out for. Instead of disregarding commas in the string, I should have been keeping track of them with a counter variable. That way I can flag my if statements to look for the counter variable and assign whatever was logged into the cell.

--- lab308-3.mjs
+++ lab308-3.mjs
@@ -22,7 +22,7 @@ while(prime != true){ //set up while loop for when prime is not true.
     num++;//start num count to check a few numbers after n.
     prime = true; //debugger found this to be problematic. I'm not sure why, but I initially put this at the end of the loop. 
     for(let i = 2; i <= 10; i++) //initially I wanted to do an if statement first, and nest a for loop within it, but it got a bit complicated. I was advised to put the for loop above the if statement.
-    {//for loop that would create the variable i, so that i can be checked with num to find a remainder. i will continue to increase by one until it reaches ten or if...
+    {   //for loop that would create the variable i, so that i can be checked with num to find a remainder. i will continue to increase by one until it reaches ten or if...
         if(num % i === 0){ //if num % i is exactly 0, then i will stop counting
             prime = false; // then the prime bool will switch back to false
             break; //break out of the nested for loop, since the for loop has done it's job to find a number (i) that is divisible by the var num other than 1. 
@@ -42,20 +42,33 @@ let cell2='';
 let cell3 ='';
 let cell4 ='';
 let c =''; //empty string reader
-const csv = "ID,Name,Occupation,Age\n42,";
+let count = 1; //empty counter var
+const csv = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 
-//try to do a for loop within a for loop. because I want to stop after the comma, but continue with the string until all cells are filled. 
-// the main for loop should look something like this: for (let i = 0; i < csv.length; i++)
-
-
+//create a loop that has c go through the string, logging in the texts. if c encounters a comma, uptick the counter. Then check to see if the counter is at a specific number.
+//Once count reaches a number, print whatever c has logged into the cell. 
+// if encounter a \n, reset count to 1 and continue with loop. 
 
 for (c of csv) { //for loop with string c of csv reading through the string csv.
-    if (c !== `,`) { //have the string c go through the csv string until it reaches a comma.
-        cell1 += c; //Concatenate and asign cell1 with string c. 
-        c = ``; //reset string c to zero.
-    } else{break;}
+    if (c === `,`) { //have the string c go through the csv string until it reaches a comma.
+        count++; //uptick the counter when c has encountered the comma. 
+    } else if(c === `\n`){ //if c encounters \n
+        count = 1; //reset count
+        continue; //countinue with loop
+    } else{
+        if (count === 1) {
+            cell1 += c; //Concatenate and asign cell1 with string c since count = 1. 
+        } else if (count === 2) {
+            cell2 += c; //Concatenate and asign cell2 with string c since count = 2. 
+        } else if (count === 3) {
+            cell3 += c; //Concatenate and asign cell3 with string c since count = 3. 
+        }  else if (count === 4) {
+            cell4 += c; //Concatenate and asign cell4 with string c since count = 4. 
+        } 
+        
+    }
 
 }
    
 
-console.log(cell1, cell2);
+console.log(cell1, cell2, cell3, cell4);
